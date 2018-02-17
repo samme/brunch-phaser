35856e53fd5c244ec6721953a00d7605041a6ed7
samme <samme@users.noreply.github.com>
2018-02-17 10:54:55 -0800

    Extract to scenes/default

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,4 @@
-var config = {
+window.game = new Phaser.Game({
 
   type: Phaser.AUTO,
 
@@ -23,34 +23,6 @@ var config = {
     background: ['#e54661', '#ffa644', '#998a2f', '#2c594f', '#002d40']
   },
 
-  scene: {
+  scene: require('scenes/default')
 
-    preload: function () {
-      this.load.setPath('assets/');
-      this.load.image('sky', 'space3.png');
-      this.load.image('logo', 'phaser3-logo.png');
-      this.load.image('red', 'red.png');
-      // this.load.on('progress', console.log);
-    },
-
-    create: function () {
-      var sky = this.add.image(400, 300, 'sky');
-      sky.alpha = 0.5;
-      var particles = this.add.particles('red');
-      var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-      });
-      var logo = this.physics.add.image(400, 100, 'logo');
-      logo.setVelocity(100, 200);
-      logo.setBounce(1, 1);
-      logo.setCollideWorldBounds(true);
-      emitter.startFollow(logo);
-    }
-
-  }
-
-};
-
-window.game = new Phaser.Game(config);
+});
---
+++ app/scenes/default.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+  preload: function () {
+    this.load.setPath('assets/');
+    this.load.image('sky', 'space3.png');
+    this.load.image('logo', 'phaser3-logo.png');
+    this.load.image('red', 'red.png');
+    // this.load.on('progress', console.log);
+  },
+
+  create: function () {
+    var sky = this.add.image(400, 300, 'sky');
+    sky.alpha = 0.5;
+    var particles = this.add.particles('red');
+    var emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: 'ADD'
+    });
+    var logo = this.physics.add.image(400, 100, 'logo');
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+    emitter.startFollow(logo);
+  }
+
+};
