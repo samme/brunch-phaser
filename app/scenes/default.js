d8c058f907a571e9d05345c71490ad89ed97bccf
samme <samme@users.noreply.github.com>
2018-02-25 15:55:54 -0800

    Fancy progress bar

--- app/scenes/default.js
+++ app/scenes/default.js
@@ -8,9 +8,9 @@ module.exports = {
     this.load.image('sky', 'space3.png');
     this.load.image('logo', 'phaser3-logo.png');
     this.load.image('red', 'red.png');
-    this.progressBar = this.add.graphics(0, 0);
     this.load.on('progress', this.onLoadProgress, this);
     this.load.on('complete', this.onLoadComplete, this);
+    this.createProgressBar();
   },
 
   create: function () {
@@ -33,16 +33,29 @@ module.exports = {
 
     progressBar: null,
 
-    onLoadComplete: function () { // (loader, storageSize, failedSize)
-      console.log('onLoadComplete');
+    progressBarRectangle: null,
+
+    createProgressBar: function () {
+      var main = this.cameras.main;
+      this.progressBarRectangle = new Phaser.Geom.Rectangle(0, 0, 0.5 * main.width, 50);
+      Phaser.Geom.Rectangle.CenterOn(this.progressBarRectangle, 0.5 * main.width, 0.5 * main.height);
+      this.progressBar = this.add.graphics();
+    },
+
+    onLoadComplete: function (loader) {
+      console.log('onLoadComplete', loader);
       this.progressBar.destroy();
     },
 
     onLoadProgress: function (progress) {
+      var rect = this.progressBarRectangle;
+      var color = (this.load.failed.size > 0) ? (0xff2200) : (0xffffff);
       this.progressBar
         .clear()
-        .fillStyle(0xffffff, 0.75)
-        .fillRect(0, 0, 800 * progress, 50);
+        .fillStyle(0x222222)
+        .fillRect(rect.x, rect.y, rect.width, rect.height)
+        .fillStyle(color)
+        .fillRect(rect.x, rect.y, progress * rect.width, rect.height);
       console.log('progress', progress);
     }
 
